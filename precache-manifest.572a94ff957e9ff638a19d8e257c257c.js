4fd6df364c2c104ad602285af888ebcec2c89da0
Kim Jibin <a5817028@aoyama.jp>
2022-03-08 21:05:34 +0900

    Updates

---
+++ precache-manifest.572a94ff957e9ff638a19d8e257c257c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a476240e355697e3886e145496662a09",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "c6fbed28d0ec60c1f085",
+    "url": "/portfolio-website/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "7880305cec66b71236b1",
+    "url": "/portfolio-website/static/css/main.a299c7a9.chunk.css"
+  },
+  {
+    "revision": "c6fbed28d0ec60c1f085",
+    "url": "/portfolio-website/static/js/2.32fb277d.chunk.js"
+  },
+  {
+    "revision": "3c1d577cdee30e9828f6470251853e49",
+    "url": "/portfolio-website/static/js/2.32fb277d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7880305cec66b71236b1",
+    "url": "/portfolio-website/static/js/main.fea6e8da.chunk.js"
+  },
+  {
+    "revision": "3d6653802d54a30f29c9",
+    "url": "/portfolio-website/static/js/runtime-main.b5bf659a.js"
+  },
+  {
+    "revision": "f287a42a71ecf496cca0c186425c052f",
+    "url": "/portfolio-website/static/media/profile.f287a42a.jpg"
+  },
+  {
+    "revision": "9797e39eae34809af2e63138259c2808",
+    "url": "/portfolio-website/static/media/profileEmoji.9797e39e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d6e4a0f2ecdbeeac1266b8111a0f3fc.js"
+  "/portfolio-website/precache-manifest.572a94ff957e9ff638a19d8e257c257c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
